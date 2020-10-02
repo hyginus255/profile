1ac6d1c4f258e4c072250b32426574b5c3b02d8f
Hyginus Ukeh <33868946+hyginus255@users.noreply.github.com>
2020-10-02 09:24:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
 	<link rel="stylesheet" href="css/animate.css" type="text/css" media="all">
 	<link rel="stylesheet" href="css/magnific-popup.css" type="text/css" media="all">
 	<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+	<script data-ad-client="ca-pub-2211212453223346" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 	
 	<style>
 		#profile-image{
